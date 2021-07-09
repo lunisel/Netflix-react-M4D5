01a46c99adaa1de7a589981b398ac95ef36bf2f2
aizada869-nazarova <Aizakoni29@gmail.com>
2021-07-09 16:08:25 +0600

    Comments

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import AddComments from './AddComments'
 
 function App() {
-  return (
-   <div>
-     
-   </div>
-  );
+    return ( < div >
+
+        <
+        AddComments / >
+        <
+        /div>
+    );
 }
 
 export default App;
