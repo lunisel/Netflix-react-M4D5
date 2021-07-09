f267d245fd300d0e4cad7e50b5d7a91853169ee7
pavan kumar <pavan.yalamanchi@gmail.com>
2021-07-09 15:33:48 +0100

    Merging navbars and footer to main

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import FetchMovies from "./Components/FetchMovies";
 import CommentsArea from "./CommentArea";
+import NavBar from './Components/NavBar'
+import UnderNavbar from './Components/UnderNavbar'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <>
+    <NavBar/>
+    <UnderNavbar/>
       <FetchMovies saga="harry%20potter" />
       <FetchMovies saga="lord%20of%20the%20rings" />
       <FetchMovies saga="avengers" />
       <CommentsArea />
+      <Footer/>
     </>
   );
 }
