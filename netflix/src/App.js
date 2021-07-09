177169f2d0dfebf691d8c1f23483cb3ea4953adf
Luna Sarmiento <selene942008@hotmail.com>
2021-07-09 16:30:14 +0200

    added h3

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -5,9 +5,12 @@ import CommentsArea from "./CommentArea";
 function App() {
   return (
     <>
-      <FetchMovies saga="harry%20potter" />
-      <FetchMovies saga="lord%20of%20the%20rings" />
-      <FetchMovies saga="avengers" />
+      <FetchMovies saga="harry%20potter" title="Harry Potter Saga" />
+      <FetchMovies
+        saga="lord%20of%20the%20rings"
+        title="Lord of the Rings Saga"
+      />
+      <FetchMovies saga="avengers" title="The Avengers Saga" />
       <CommentsArea />
     </>
   );
